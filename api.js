d8141e100e932078091bc4d80e8dcda6f65f11ef
jilv220 <LyuJi220@hotmail.com>
2022-05-03 18:05:03 -0400

    config address base

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(compression())
 
 // Api config
-const base = 'http://localhost'
+const base = process.env.BASE || 'http://localhost'
 const port = process.env.PORT || 5000
 const lbryPort = 5279
 const lbryUrl = `${base}:${lbryPort}`
