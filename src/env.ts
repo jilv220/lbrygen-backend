7d4bf4bff4b1415da3c5287c14100db52316dd24
jilv220 <LyuJi220@hotmail.com>
2022-08-26 20:02:22 -0400

    update domain

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-export const API_PROD = 'https://lbrygen-backend.xyz/api'
-export const API_BASE = 'https://lbrygen-backend.xyz/'
+export const API_PROD = 'https://lbrygen.xyz/api'
+export const API_BASE = 'https://lbrygen.xyz/'
 export const PROD_IP = 'http://161.97.103.194/'
 export const STREAM_IP = 'http://127.0.0.1:5280/'
 
